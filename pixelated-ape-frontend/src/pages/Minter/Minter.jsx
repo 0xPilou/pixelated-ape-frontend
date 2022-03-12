1de59a8c0b82f47e2bd3952481f0798d070747d5
PierreLvl <pierre.louvel@protonmail.com>
2022-03-12 10:24:47 +0100

    Minter style reorg

--- pixelated-ape-frontend/src/pages/Minter/Minter.jsx
+++ pixelated-ape-frontend/src/pages/Minter/Minter.jsx
@@ -10,8 +10,7 @@ import FetchData from '../../helpers/FetchData'
 import backgroundVideo from '../../images/background.mp4';
 import gif from '../../images/gif/preview450.gif';
 
-
-
+import './Minter.css'
 
 function Minter() {
 
@@ -26,59 +25,51 @@ function Minter() {
     return () => { isMounted = false };
   }, [mint])
 
-
-
   return <>
     {/* <video autoPlay loop muted className='video'>
       <source src={backgroundVideo} type='video/mp4' />
     </video> */}
-    <section className="minting">
+    <section className="minter">
       <Grid container spacing={2} justifyContent="center" alignItems="center">
         <Grid item xs={4}>
-          <div className="gif">
-            <img src={gif} alt="CollectionGif" />
+          <div>
+            <img src={gif} />
           </div>
         </Grid>
         <Grid item xs={6}>
           <Grid container direction="column" justifyContent="center" alignItems="center">
-            <p className='minting-wd-title'>Mint a Pixelated Ape </p>
-            <p className='minting-wd-subtitle'>Mint up to 10 APG and become a community member</p>
-
-
+            <p className='minter-title'>Mint a Pixelated Ape </p>
+            <p className='minter-subtitle'>Mint up to 10 APG and become a community member</p>
             <Grid container spacing={2} justifyContent="center" alignItems="center">
               {(connectionStatus === null
                 || connectionStatus === "disconnected"
-                || connectionStatus === "") && <p className='minting-wd-title'>Connect Wallet</p>}
+                || connectionStatus === "") && <p className='minter-title'>Connect Wallet</p>}
               {connectionStatus === "connected" &&
                 <>
-                  <p className='minting-wd-title'>{data.totalSupply} / {data.maxQuantity}</p>
-                  <p className='minting-wd-subtitle'>Each Pixelated Ape costs {data.cost / 10 ** 18} ETH</p>
+                  <p className='minter-title'>{data.totalSupply} / {data.maxQuantity}</p>
+                  <p className='minter-subtitle'>Each Pixelated Ape costs {data.cost / 10 ** 18} ETH</p>
                   <Grid container spacing={2} justifyContent="center" alignItems="center">
 
-                    <button className="mint-btn btn" onClick={decreaseNumber}>
+                    <button className="minter-btn btn" onClick={decreaseNumber}>
                       <span>-</span>
                     </button>
-                    <button className="mint-btn num">
+                    <button className="minter-btn num">
                       <span>{number}</span>
                     </button>
-                    <button className="mint-btn btn" onClick={increaseNumber}>
+                    <button className="minter-btn btn" onClick={increaseNumber}>
                       <span>+</span>
                     </button>
-                    <button className="mint-btn btn" onClick={mint}>
+                    <button className="minter-btn btn" onClick={mint}>
                       <span>Mint</span>
                     </button>
                   </Grid>
                 </>
               }
-
             </Grid>
           </Grid>
-
         </Grid>
       </Grid>
     </section>
   </>
-}
-
-export default Minter
+} export default Minter
 
