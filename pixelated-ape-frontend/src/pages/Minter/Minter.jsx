0842f3ba60d1a5905baf4e656db38507acdc5cba
PierreLvl <pierre.louvel@protonmail.com>
2022-03-12 09:19:38 +0100

    update minter

--- pixelated-ape-frontend/src/pages/Minter/Minter.jsx
+++ pixelated-ape-frontend/src/pages/Minter/Minter.jsx
@@ -6,6 +6,8 @@ import { ethers } from 'ethers'
 /* Logic & Helpers */
 import MinterLogic from './MinterLogic'
 import FetchData from '../../helpers/FetchData'
+import Wallet from '../../helpers/Wallet'
+
 
 /* UI Elements */
 import backgroundVideo from '../../images/background.mp4';
@@ -16,11 +18,13 @@ import CONTRACT from '../../contract/ApePixelGang.json'
 import CONFIG from '../../config.json'
 
 
-function Minter({ connected }) {
+function Minter() {
 
   const { number, increaseNumber, decreaseNumber } = MinterLogic()
   const { data, fetchData } = FetchData();
   const [error, setError] = useState('');
+
+  const connectionStatus = localStorage.getItem('status')
 
   useEffect(() => {
     fetchData();
@@ -63,34 +67,34 @@ function Minter({ connected }) {
         <Grid item xs={6}>
           <Grid container direction="column" justifyContent="center" alignItems="center">
             <p className='minting-wd-title'>Mint a Pixelated Ape </p>
-            <p className='minting-wd-subtitle'>
-              Ape Pixel Gang is a Bored Ape Yacht Club Derivative Collection.
-            </p>
-            <p className='minting-wd-subtitle'>
-              Each one of the 10k BAYC has been carefully pixelated with a random pixel size.
-            </p>
             <p className='minting-wd-subtitle'>Mint up to 10 APG and become a community member</p>
-            <p className='minting-wd-title'>{data.totalSupply} / {data.maxQuantity}</p>
-            <p className='minting-wd-subtitle'>Each Pixelated Ape costs {data.cost / 10 ** 18} ETH</p>
+
 
             <Grid container spacing={2} justifyContent="center" alignItems="center">
-              {connected === false && <p className='warning'>Connect Wallet</p>}
-              {connected === true && <Grid container spacing={2} justifyContent="center" alignItems="center">
+              {(connectionStatus === null
+                || connectionStatus === "disconnected"
+                || connectionStatus === "") && <p className='minting-wd-title'>Connect Wallet</p>}
+              {connectionStatus === "connected" &&
+                <>
+                  <p className='minting-wd-title'>{data.totalSupply} / {data.maxQuantity}</p>
+                  <p className='minting-wd-subtitle'>Each Pixelated Ape costs {data.cost / 10 ** 18} ETH</p>
+                  <Grid container spacing={2} justifyContent="center" alignItems="center">
 
-                <button className="mint-btn btn" onClick={decreaseNumber}>
-                  <span>-</span>
-                </button>
-                <button className="mint-btn num">
-                  <span>{number}</span>
-                </button>
-                <button className="mint-btn btn" onClick={increaseNumber}>
-                  <span>+</span>
-                </button>
-                <button className="mint-btn btn" onClick={mint}>
-                  <span>Mint</span>
-                </button>
-              </Grid>}
-
+                    <button className="mint-btn btn" onClick={decreaseNumber}>
+                      <span>-</span>
+                    </button>
+                    <button className="mint-btn num">
+                      <span>{number}</span>
+                    </button>
+                    <button className="mint-btn btn" onClick={increaseNumber}>
+                      <span>+</span>
+                    </button>
+                    <button className="mint-btn btn" onClick={mint}>
+                      <span>Mint</span>
+                    </button>
+                  </Grid>
+                </>
+              }
 
             </Grid>
           </Grid>
