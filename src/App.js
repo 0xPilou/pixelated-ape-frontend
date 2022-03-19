e8fb475cdf62b264fd95b397cc1913911e92011e
PierreLvl <pierre.louvel@protonmail.com>
2022-03-19 11:51:17 +0100

    reorg folder

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 /* Libs & Modules */
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { Navbar } from './components/Navbar'
+import { Navbar } from './components/Navbar/Navbar'
 
 /* Pages & Components */
 import Minter from './pages/Minter/Minter'
