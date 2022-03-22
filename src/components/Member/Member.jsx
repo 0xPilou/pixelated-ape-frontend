4474be92dcae14b49617192303acff372959c8d9
PierreLvl <pierre.louvel@protonmail.com>
2022-03-22 13:03:10 +0100

    team and member components

---
+++ src/components/Member/Member.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Card, CardHeader, CardContent, CardActions, Grid, CardMedia, Typography } from '@material-ui/core';
+import './Member.css';
+
+function Member({ name, role, img, twitter }) {
+    return (
+        <Grid item xs={12} sm={8} md={5}>
+            <a href={twitter} target="_blank" className="member__socials">
+
+                <Card className='member__card'>
+                    <CardHeader
+                        className='member__card__header'
+                        title={name}
+                        subheader={role}
+                    />
+                    <CardContent>
+
+                        <CardMedia
+                            component="img"
+                            height="300"
+                            image={img}
+                            alt={name}
+                        />
+                    </CardContent>
+
+                    <CardActions disableSpacing className='member__contact'>
+                    </CardActions>
+                </Card>
+            </a>
+        </Grid>
+    )
+}
+
+export default Member
---
+++ src/components/Team/Team.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Grid } from '@material-ui/core';
+
+import Member from '../Member/Member';
+import './Team.css'
+import pilou from '../../images/team/pilou.png'
+import bennyblanco from '../../images/team/bennyblanco.png'
+import baptman from '../../images/team/baptman.png'
+
+
+function Team() {
+    return (
+        <Grid container spacing={8} className="team__grid">
+            <Member 
+                name='0xPilou'
+                role='Lead Dev'
+                img={pilou}
+                twitter="https://twitter.com/0xPilou"
+            />
+            <Member 
+                name='Benny Blanco'
+                role='Communication & Marketing'
+                img={bennyblanco}
+                twitter="https://twitter.com/BennyBlanco_OG"
+            />
+            <Member
+                name='Bapt Man'
+                role='Graphic Designer'
+                img={baptman}
+                twitter="https://twitter.com/_bapt_man_"
+            />
+        </Grid>
+    )
+}
+
+export default Team
