1e92c5cf498c33b4b5c63b6e72e1c7f2cc18fcd5
PierreLvl <pierre.louvel@protonmail.com>
2022-03-22 13:04:06 +0100

    add team to about page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -2,6 +2,9 @@
 import React from "react";
 import { Grid } from '@material-ui/core';
 
+/* Pages & Components */
+import Team from '../../components/Team/Team'
+
 /* UI Elements */
 import backgroundVideo from '../../images/background.mp4';
 import about_gif from '../../images/gif/about_gif.gif';
@@ -26,18 +29,22 @@ function About() {
           <Grid className="about__collection__description__text">
             <p className='about__collection__title'>THE COLLECTION</p>
             <p className='about__collection__text'>
-            {CONFIG.ABOUT_COLLECTION_1}
+              {CONFIG.ABOUT_COLLECTION_1}
             </p>
             <p className='about__collection__text'>
-            {CONFIG.ABOUT_COLLECTION_2}
+              {CONFIG.ABOUT_COLLECTION_2}
 
             </p>
           </Grid>
         </Grid>
         <Grid className="about__collection__gif">
-            <img src={about_gif} className="about__collection__gif__img"/>
+          <img src={about_gif} className="about__collection__gif__img" />
         </Grid>
       </Grid>
+      <Grid>
+        <h1 className='about__header'>ABOUT THE TEAM</h1>
+      </Grid>
+      <Team />
     </div >
   </>
 }
