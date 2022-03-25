3ca98ed68baddb5a817173e89c6e0b65a5e73930
PierreLvl <pierre.louvel@protonmail.com>
2022-03-25 14:46:54 +0100

    rework styling

--- src/pages/Minter/Minter.jsx
+++ src/pages/Minter/Minter.jsx
@@ -16,7 +16,7 @@ import './Minter.css'
 
 function Minter() {
 
-  const { number, increaseNumber, decreaseNumber, mint } = MinterLogic()
+  const { mint } = MinterLogic()
   const { data, fetchData } = FetchData();
 
   const connectionStatus = localStorage.getItem('status')
@@ -32,26 +32,38 @@ function Minter() {
       <source src={backgroundVideo} type='video/mp4' />
     </video> */}
     <div className="main">
-      <Grid className="minter__grid" container spacing={2} justifyContent="center" alignItems="center">
+      <Grid className="minter__grid" container spacing={2}>
         <Grid className="minter__grid__gif">
-          <div>
-            <img src={minter_gif} className="minter__grid__gif__img" />
-          </div>
+          <p className='minter__title'>Mint a Pixelated Ape </p>
+
+          <p className='minter__subtitle'>Mint up to <span className="minter__info">10 APG per tx </span></p>
+          <p className='minter__title'>
+          <span className="minter__info">{data.totalSupply}</span> / {data.maxQuantity}
+          </p>
+          <Grid container direction="column">
+            <Grid item>
+              <p className='minter__subtitle__price'>Each piece costs :</p>
+            </Grid>
+            <Grid item>
+              <p className='minter__subtitle__price'><span className="minter__info">{data.cost / 10 ** 18} ETH</span></p>
+            </Grid>
+            <Grid item>
+              <p className='minter__subtitle__price'>or</p>
+            </Grid>
+            <Grid item>
+              <p className='minter__subtitle__price'><span className="minter__info">15 APECOIN</span></p>
+            </Grid>
+          </Grid>
+            
         </Grid>
         <Grid className="minter__grid__action">
           <Grid>
-            <p className='minter__title'>Mint a Pixelated Ape </p>
-            <p className='minter__subtitle'>Mint up to 10 APG and become a community member</p>
             <Grid container spacing={2} justifyContent="center" alignItems="center">
               {(connectionStatus === null
                 || connectionStatus === "disconnected"
                 || connectionStatus === "") && <p className='minter__title'>Connect Wallet</p>}
               {connectionStatus === "connected" &&
                 <>
-                  <p className='minter__title'>
-                    {data.totalSupply} / {data.maxQuantity}
-                  </p>
-                  <p className='minter__subtitle'>Each piece costs {data.cost / 10 ** 18} ETH or 15 APECOIN</p>
                   <MintButton />
                 </>
               }
