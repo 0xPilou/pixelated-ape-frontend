e79f25b407b5a8e4b9996129377e14a62e2ce780
PierreLvl <pierre.louvel@protonmail.com>
2022-04-01 15:48:21 +0200

    member rework

--- src/components/Member/Member.jsx
+++ src/components/Member/Member.jsx
@@ -4,27 +4,27 @@ import './Member.css';
 
 function Member({ name, role, img, twitter }) {
     return (
-        <Grid item xs={12} sm={8} md={5}>
-            <a href={twitter} target="_blank" className="member__socials">
-                <Card className='member__card'>
-                    <CardMedia
-                        component="img"
-                        height="400"
-                        image={img}
-                        alt={name}
-                    />
-                    <CardContent>
-                        <CardHeader
-                            className='member__card__header'
-                            title={name}
-                            subheader={role}
+            <Grid item xs={12} sm={8} md={5}>
+                <a href={twitter} target="_blank" className="member__socials">
+                    <Card className='member__card'>
+                        <CardMedia
+                            component="img"
+                            height="400"
+                            image={img}
+                            alt={name}
                         />
-                    </CardContent>
-                    <CardActions disableSpacing className='member__contact'>
-                    </CardActions>
-                </Card>
-            </a>
-        </Grid>
+                        <CardContent>
+                            <CardHeader
+                                className='member__card__header'
+                                title={name}
+                                subheader={role}
+                            />
+                        </CardContent>
+                        <CardActions disableSpacing className='member__contact'>
+                        </CardActions>
+                    </Card>
+                </a>
+            </Grid>
     )
 }
 
